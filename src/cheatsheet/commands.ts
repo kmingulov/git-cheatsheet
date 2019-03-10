6cf60fb186f43f513c316a2f8a3f74b26725bd6a
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2019-03-10 16:48:36 +0700

    Add first version of Git commands

---
+++ src/cheatsheet/commands.ts
@@ -0,0 +1,32 @@
+import { Command } from './command/Command';
+import { CommandGroup } from './command/CommandGroup';
+import { command, url, file } from './command/CommandPart';
+
+const CLONE_AND_REMOTES: CommandGroup = new CommandGroup(
+  'Clone & Remotes',
+  [
+    new Command(
+      '',
+      [ command('clone'), url('https://github.com/some-user/some-repo'), file('dir') ],
+      [ 'Clone an existing repository by its URL to ', file('dir'), '.' ]
+    ),
+
+    new Command(
+      '',
+      [ command('remote'), '-v' ],
+      [ 'List all existing remotes with their URLs.' ]
+    ),
+
+    new Command(
+      '',
+      [ command('remote'), 'add', url('https://github.com/some-user/some-repo') ],
+      ['Add a new remote by its URL.']
+    )
+  ]
+);
+
+const COMMAND_GROUPS: CommandGroup[] = [
+  CLONE_AND_REMOTES
+];
+
+export default COMMAND_GROUPS;
