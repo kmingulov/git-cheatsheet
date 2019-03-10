2ff42b988c60f153bf207bd0d89839dad3cf7eb9
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2019-03-10 16:48:55 +0700

    Add NavBar and CommandTable to App

--- src/cheatsheet/App.tsx
+++ src/cheatsheet/App.tsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { Container, Navbar } from 'react-bootstrap';
+
+import CommandTable from './command-table/CommandTable';
+import commands from './commands';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Under construction.
+        <Navbar bg='primary' sticky='top'>
+          <Container className='header'>
+            <Navbar.Brand>Git Cheatsheet</Navbar.Brand>
+          </Container>
+        </Navbar>
+
+        <Container className='mainContent'>
+          <CommandTable groups={ commands }/>
+        </Container>
       </div>
     );
   }
