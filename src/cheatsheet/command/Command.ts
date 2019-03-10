6035ee642560af0fed9164ee26e122e12c1954de
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2019-03-10 17:36:43 +0700

    Make scmBreezeShortcut optional

--- src/cheatsheet/command/Command.ts
+++ src/cheatsheet/command/Command.ts
@@ -1,15 +1,15 @@
 import { CommandPart } from './CommandPart';
 
 export class Command {
-  public readonly scmBreezeShortcut: Array<CommandPart | string>;
   public readonly command: Array<CommandPart | string>;
   public readonly description: Array<CommandPart | string>;
+  public readonly scmBreezeShortcut: Array<CommandPart | string>;
 
-  constructor(scmBreezeShortcut: Array<CommandPart | string>,
-              commandParts: Array<CommandPart | string>,
-              descriptionParts: Array<CommandPart | string>) {
-    this.scmBreezeShortcut = scmBreezeShortcut;
+  constructor(commandParts: Array<CommandPart | string>,
+              descriptionParts: Array<CommandPart | string>,
+              scmBreezeShortcut?: Array<CommandPart | string>) {
     this.command = commandParts;
     this.description = descriptionParts;
+    this.scmBreezeShortcut = scmBreezeShortcut || [];
   }
 }
