866dd3bdf7fdcbb182d5ed31aeffb31ecd956950
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2019-03-11 21:40:03 +0700

    Add a unique ID to each command

--- src/cheatsheet/command/Command.ts
+++ src/cheatsheet/command/Command.ts
@@ -1,13 +1,16 @@
 import { CommandPart } from './CommandPart';
 
 export class Command {
+  public readonly id: string;
   public readonly command: Array<CommandPart | string>;
   public readonly description: Array<CommandPart | string>;
   public readonly scmBreezeShortcut: Array<CommandPart | string>;
 
-  constructor(commandParts: Array<CommandPart | string>,
+  constructor(id: string,
+              commandParts: Array<CommandPart | string>,
               descriptionParts: Array<CommandPart | string>,
               scmBreezeShortcut?: Array<CommandPart | string>) {
+    this.id = id;
     this.command = commandParts;
     this.description = descriptionParts;
     this.scmBreezeShortcut = scmBreezeShortcut || [];
