6080e674a97be34001c5bc69feb9195e05e43f45
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2019-03-13 22:56:02 +0700

    Add tests for commands list

---
+++ src/cheatsheet/command.test.ts
@@ -0,0 +1,24 @@
+import { CommandGroup } from './command/CommandGroup';
+import commands from './commands';
+
+describe('Commands list', () => {
+
+  it('is a list of CommandGroup', () => {
+    expect(commands instanceof Array).toBe(true);
+    commands.forEach(group => {
+      expect(group instanceof CommandGroup).toBe(true);
+    });
+  });
+
+  it('has no duplicated IDs', () => {
+    const allCommands = commands
+      .map(group => group.commands)
+      .reduce((a, b) => a.concat(b));
+
+    const ids = allCommands.map(command => command.id);
+    const uniqueIds = ids.filter((id, index) => ids.indexOf(id) === index);
+
+    expect(uniqueIds).toEqual(ids);
+  });
+
+});
