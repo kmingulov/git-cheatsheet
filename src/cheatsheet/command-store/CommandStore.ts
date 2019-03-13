55d386cc9c58c7e18e44b45109b7f00d98343ef7
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2019-03-13 22:02:37 +0700

    TA679674 Use lunr.js for search

--- src/cheatsheet/command-store/CommandStore.ts
+++ src/cheatsheet/command-store/CommandStore.ts
@@ -1,36 +1,84 @@
+import { Builder, Index, PipelineFunction, Query } from 'lunr';
+
 import { Command } from '../command/Command';
 import { CommandGroup } from '../command/CommandGroup';
-import { CommandPart } from '../command/CommandPart';
+import { SearchableCommand } from './SearchableCommand';
+
+const IGNORED_TOKEN_CHARS = /\W+/;
+
+const getAllCommands = (groups: CommandGroup[]): Command[] => {
+  return groups
+    .map(group => group.commands)
+    .reduce((a, b) => a.concat(b), []);
+};
+
+interface CommandByIdHash {
+  [ id: string ]: Command;
+}
+
+const getCommandByIdHash = (commands: Command[]): CommandByIdHash => {
+  return commands.reduce((hash, cmd) => {
+    hash[cmd.id] = cmd;
+    return hash;
+  }, {} as CommandByIdHash);
+};
+
+const trimWord = (word: string): string => word.replace(IGNORED_TOKEN_CHARS, '');
+const trimToken: PipelineFunction = token => token.update(trimWord);
+
+const buildSearchIndex = (commands: Command[]): Index => {
+  const builder = new Builder();
+
+  builder.ref('id');
+  builder.field('command');
+  builder.field('description', { boost: 10 });
+  builder.field('scmBreezeShortcut');
+
+  builder.tokenizer.separator = IGNORED_TOKEN_CHARS;
+  builder.pipeline.add(trimToken);
+  builder.searchPipeline.add(trimToken);
+
+  builder.metadataWhitelist = [ 'position' ];
+
+  commands.forEach(cmd => builder.add(new SearchableCommand(cmd)));
+
+  return builder.build();
+};
 
 export class CommandStore {
   private readonly commandGroups: CommandGroup[];
+  private readonly commandById: CommandByIdHash;
+  private readonly searchIndex: lunr.Index;
 
   constructor(commandGroups: CommandGroup[]) {
     this.commandGroups = commandGroups;
+
+    const commands = getAllCommands(commandGroups);
+    this.commandById = getCommandByIdHash(commands);
+    this.searchIndex = buildSearchIndex(commands);
   }
 
   public getAll(): CommandGroup[] {
     return this.commandGroups;
   }
 
-  public search(searchTerm: string): Command[] {
-    return this.commandGroups
-      .map(group => this.searchCommands(searchTerm, group.commands))
-      .reduce((a, b) => a.concat(b), []);
-  }
+  public search(searchQuery: string): Command[] {
+    const terms = searchQuery
+      .split(IGNORED_TOKEN_CHARS)
+      .map(trimWord)
+      .filter(token => token !== '')
+      .filter((value, i, array) => array.indexOf(value) === i);
 
-  private searchCommands(searchTerm: string, commands: Command[]): Command[] {
-    return commands.filter(command => this.commandMatches(searchTerm, command));
-  }
+    const results = this.searchIndex.query(q => {
+      // Exact match.
+      q.term(terms, { boost: 100 });
+      // Prefix match without stemmer.
+      q.term(terms, { usePipeline: false, wildcard: Query.wildcard.TRAILING, boost: 10 });
+      // Fuzzy match.
+      q.term(terms, { usePipeline: false, editDistance: 1, boost: 1 });
+    });
 
-  private commandMatches(searchTerm: string, command: Command): boolean {
-    return command.command.some(part => this.partMatches(searchTerm, part))
-      || command.description.some(part => this.partMatches(searchTerm, part))
-      || command.scmBreezeShortcut.some(part => this.partMatches(searchTerm, part));
-  }
-
-  private partMatches(searchTerm: string, part: CommandPart | string): boolean {
-    const text = typeof(part) === 'string' ? part : part.text;
-    return text.toLocaleLowerCase().indexOf(searchTerm.toLocaleLowerCase()) !== -1;
+    return results
+      .map(searchResult => this.commandById[searchResult.ref]);
   }
 }
--- src/cheatsheet/command-store/SearchableCommand.ts
+++ src/cheatsheet/command-store/SearchableCommand.ts
@@ -4,16 +4,19 @@ import { CommandPart } from '../command/CommandPart';
 const partToString = (part: CommandPart | string): string =>
   (typeof part === 'string') ? part : part.text;
 
+const partsToString = (parts: Array<CommandPart | string>, delimeter: string): string =>
+  parts.map(partToString).join(delimeter);
+
 export class SearchableCommand {
   public readonly id: string;
-  public readonly command: string[];
-  public readonly description: string[];
-  public readonly scmBreezeShortcut: string[];
+  public readonly command: string;
+  public readonly description: string;
+  public readonly scmBreezeShortcut: string;
 
   constructor(command: Command) {
     this.id = command.id;
-    this.command = command.command.map(partToString);
-    this.description = command.description.map(partToString);
-    this.scmBreezeShortcut = command.scmBreezeShortcut.map(partToString);
+    this.command = partsToString(command.command, ' ');
+    this.description = partsToString(command.description, '');
+    this.scmBreezeShortcut = partsToString(command.scmBreezeShortcut, ' ');
   }
 }
