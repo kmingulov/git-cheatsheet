37063280eb5847e8c9ee387f6ac79a04c52fc78d
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2019-03-14 22:48:39 +0700

    Add CommandHighlighter

---
+++ src/cheatsheet/command-store/CommandHighlighter.ts
@@ -0,0 +1,34 @@
+import { Command } from '../command/Command';
+import { CommandPart, highlight } from '../command/CommandPart';
+import Type from '../command/CommandPartType';
+
+const highlightPart = (part: CommandPart, regexp: RegExp): CommandPart[] =>
+  part.text.split(regexp)
+    .map(item =>
+      item.match(regexp)
+        ? new CommandPart(Type.HIGHLIGHT, item)
+        : new CommandPart(part.role, item),
+    );
+
+const highlightParts = (parts: CommandPart[], regexp: RegExp): CommandPart[] =>
+  parts
+    .map(part => highlightPart(part, regexp))
+    .reduce((theseParts, otherParts) => theseParts.concat(otherParts), []);
+
+export class CommandHighlighter {
+
+  public static highlight(command: Command, terms: string[]): Command {
+    const regexp = new RegExp('(' + terms.join('|') + ')', 'i');
+
+    const highlightedCommand = highlightParts(command.command, regexp);
+    const highlightedDescription = highlightParts(command.description, regexp);
+    const highlightedShortcut = highlightParts(command.scmBreezeShortcut, regexp);
+
+    return new Command(command.id, highlightedCommand, highlightedDescription, highlightedShortcut);
+  }
+
+  public static highlightAll(commands: Command[], terms: string[]): Command[] {
+    return commands.map(command => this.highlight(command, terms));
+  }
+
+}
