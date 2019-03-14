b938e8f874e30679ad0f5438af1060d7f32da05d
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2019-03-14 23:07:51 +0700

    Add ArrayUtils

---
+++ src/cheatsheet/util/ArrayUtils.test.ts
@@ -0,0 +1,23 @@
+import { interleave } from './ArrayUtils';
+
+describe(interleave, () => {
+  it('doesn\'t change empty array', () => {
+    const actual = interleave([], 0);
+    expect(actual).toEqual([]);
+  });
+
+  it('doesn\'t change array with one element', () => {
+    const actual = interleave([1], 0);
+    expect(actual).toEqual([1]);
+  });
+
+  it('interleaves elements of int array', () => {
+    const actual = interleave([1, 2, 3], 0);
+    expect(actual).toEqual([1, 0, 2, 0, 3]);
+  });
+
+  it('interleaves elements of string array', () => {
+    const actual = interleave(['a', 'b', 'c'], ' ');
+    expect(actual).toEqual(['a', ' ', 'b', ' ', 'c']);
+  });
+});
