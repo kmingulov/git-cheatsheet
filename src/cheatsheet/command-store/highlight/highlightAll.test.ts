b45edceaa0174e8707e438c4e09839e8ddaec65f
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2019-03-17 18:12:40 +0700

    Add tests for highlightAll

---
+++ src/cheatsheet/command-store/highlight/highlightAll.test.ts
@@ -0,0 +1,38 @@
+import { Command, command, CommandPart, highlight, none } from 'cheatsheet/command';
+
+import { highlightAll } from './highlightAll';
+
+const cmd = new Command(
+  'cmd-1',
+  [ command('commit'), none(' '), none('-m'), none('message') ],
+  [ none('Some description.') ],
+  [ command('gc'), none(' '), none('-m'), none('message') ],
+);
+const highlightedCmd = new Command(
+  'cmd-1',
+  [ command('commit'), none(' '), none('-m'), highlight('message') ],
+  [ none('Some '), highlight('description'), none('.') ],
+  [ command('gc'), none(' '), none('-m'), highlight('message') ],
+);
+
+const otherCmd = new Command(
+  'cmd-2',
+  [ command('log') ],
+  [ none('Some description.') ],
+  [ command('gl') ],
+);
+const otherHighlightedCmd = new Command(
+  'cmd-2',
+  [ command('log') ],
+  [ none('Some '), highlight('description'), none('.') ],
+  [ command('gl') ],
+);
+
+const searchTerms = [ 'message', 'description' ];
+
+describe(highlightAll, () => {
+  it('highlights all commands', () => {
+    const actualCmds = highlightAll([ cmd, otherCmd ], searchTerms);
+    expect(actualCmds).toEqual([ highlightedCmd, otherHighlightedCmd ]);
+  });
+});
