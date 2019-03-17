a0873831d6c17f6e7c3d9d1a8a56f6910add6be5
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2019-03-17 16:25:20 +0700

    Extract CommandPage

--- src/cheatsheet/app/App.tsx
+++ src/cheatsheet/app/App.tsx
@@ -1,16 +1,10 @@
 import React, { Component, ReactElement } from 'react';
-import { Container, FormControl, InputGroup } from 'react-bootstrap';
-import { FaSearch } from 'react-icons/fa';
-
-import commands from 'cheatsheet/command-data/commands';
-import { CommandStore } from 'cheatsheet/command-store';
-import { CommandTable } from 'cheatsheet/command-table';
+import { Container } from 'react-bootstrap';
 
 import { AppHeader } from './AppHeader';
+import { CommandPage } from './pages';
 
 import './App.css';
-
-const store = new CommandStore(commands);
 
 const initialState = { searchTerm: '' };
 type State = Readonly<typeof initialState>;
@@ -22,21 +16,8 @@ export class App extends Component<object, State> {
     return (
       <div>
         <AppHeader/>
-
         <Container className='mainContent'>
-          <InputGroup className='searchBox'>
-            <InputGroup.Prepend>
-              <InputGroup.Text>
-                <FaSearch/>
-              </InputGroup.Text>
-            </InputGroup.Prepend>
-              <FormControl
-                placeholder='Search…'
-                onChange={ (event: any) => this.setState({ searchTerm: event.target.value }) }
-              />
-          </InputGroup>
-
-          <CommandTable store={ store } searchTerm={ this.state.searchTerm }/>
+          <CommandPage/>
         </Container>
       </div>
     );
---
+++ src/cheatsheet/app/pages/CommandPage.tsx
@@ -0,0 +1,35 @@
+import React, { Component, ReactElement } from 'react';
+import { FormControl, InputGroup } from 'react-bootstrap';
+import { FaSearch } from 'react-icons/fa';
+
+import commands from 'cheatsheet/command-data/commands';
+import { CommandStore } from 'cheatsheet/command-store';
+import { CommandTable } from 'cheatsheet/command-table';
+
+const store = new CommandStore(commands);
+
+type State = Readonly<{ searchTerm: string }>;
+
+export class CommandPage extends Component<object, State> {
+  public readonly state: State = { searchTerm: '' };
+
+  public render(): ReactElement {
+    return (
+      <div>
+        <InputGroup className='searchBox'>
+          <InputGroup.Prepend>
+            <InputGroup.Text>
+              <FaSearch/>
+            </InputGroup.Text>
+          </InputGroup.Prepend>
+            <FormControl
+              placeholder='Search…'
+              onChange={ (event: any) => this.setState({ searchTerm: event.target.value }) }
+            />
+        </InputGroup>
+
+        <CommandTable store={ store } searchTerm={ this.state.searchTerm }/>
+      </div>
+    );
+  }
+}
