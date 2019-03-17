ee674e333940e39d86b1df97a1e089c7784a0b11
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2019-03-17 20:21:41 +0700

    Add tests for App

---
+++ src/cheatsheet/app/App.test.tsx
@@ -0,0 +1,17 @@
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+
+import { App } from './App';
+
+jest.mock('./AppHeader');
+jest.mock('./pages/CommandPage');
+
+Enzyme.configure({adapter: new Adapter()});
+
+describe(App, () => {
+  it('renders correctly', () => {
+    const component = mount(<App/>);
+    expect(component).toMatchSnapshot();
+  });
+});
