42904c845530a5e431f33b44d043426d0f9b8d7f
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2019-03-20 21:43:49 +0700

    Add docs to util

--- src/cheatsheet/util/componentUtils/staticComponent.tsx
+++ src/cheatsheet/util/componentUtils/staticComponent.tsx
@@ -1,5 +1,11 @@
 import React, { Component, ComponentType, ReactElement } from 'react';
 
+/**
+ * Higher-order component (HOC) which wraps the given component and defines `shouldComponentUpdate` to always return
+ * `false`, so that effectively the wrapped component will never be re-rendered. This HOC is useful for fully static
+ * components whose state or properties should never change.
+ * @param WrappedComp component to wrap
+ */
 export function staticComponent<P extends object>(WrappedComp: ComponentType<P>): ComponentType<P> {
   return class StaticComponent extends Component<P> {
     public static displayName = `staticComponent(${WrappedComp.displayName || WrappedComp.name || ''})`;
