8f92e17dc0ea59346497cee2f5eddf8bfdef3d4a
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2019-03-28 22:34:53 +0700

    Add remote commands

--- src/cheatsheet/command-data/data/remote.ts
+++ src/cheatsheet/command-data/data/remote.ts
@@ -19,6 +19,21 @@ const remoteCommands: CommandGroupDescriptor = {
       description: [ 'Add remote ', remote('some-remote'), ' by its URL.' ],
       shortcut: [ command('gr'), 'add', remote('some-remote'), REPO_URL ],
     },
+    {
+      command: [ command('remote'), 'show', remote('some-remote') ],
+      description: [ 'Show remote\'s information: its URLs, main branch and remote branches.' ],
+      shortcut: [ command('gr'), 'show' ],
+    },
+    {
+      command: [ command('remote'), 'rename', remote('old-remote-name'), remote('new-remote-name') ],
+      description: [ 'Rename a remote.' ],
+      shortcut: [ command('gr'), 'rename', remote('old-remote-name'), remote('new-remote-name') ],
+    },
+    {
+      command: [ command('remote'), 'rm', remote('some-remote') ],
+      description: [ 'Remove a remote.' ],
+      shortcut: [ command('gr'), 'rm', remote('some-remote') ],
+    },
   ],
 };
 
